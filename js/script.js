8c51478a02674810c395abe4d138c63080e9e62a
Leograzioli <leograzioli@gmail.com>
2022-10-10 14:20:04 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
+const row = document.querySelector(".row");
 
 //check numbers 1 to 100 and find those can be divided by 3, 5 or both
 for (let i = 1; i <= 100; i++) {
     let result = ""
-   
+    
     if (i % 3 === 0 && i % 5 === 0) {
-        result = `Fizzbuzz`
-        console.log(result);
+        result = `fizzbuzz`
     } else if (i % 3 === 0) {
-        result = `Fizz`
-        console.log(result);
+        result = `fizz`
     } else if (i % 5 === 0){
-        result = `Buzz`
-        console.log(result);
+        result = `buzz`
     } else {
-        console.log(i);
+        result = i;
     }
+    
+    //output
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.classList.add(result)
+    card.innerHTML += result;
+    row.append(card);
 }
+
